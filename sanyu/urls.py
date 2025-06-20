21075c3aeb3a9381ea13ef6ae555c3454ad77e18
Luzinda Douglas <douglas.luzinda@gmail.com>
2025-06-20 08:35:01 -0700

    hvj

--- sanyu/urls.py
+++ sanyu/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-    path(r'^payments/', include('django_pesapal.urls')),
 ]
 
 if settings.DEBUG:
